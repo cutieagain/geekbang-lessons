a30b50e1876bdf0b3c054e2cc2ccf57845246986
cutie <cutieagain@gmail.com>
2021-03-17 23:47:23 +0800

    第三周作业部分，报错找不到类是为什么？ Exception in thread "main" java.lang.NoClassDefFoundError: javax/servlet/http/HttpServlet

--- projects/stage-0/user-platform/my-web-mvc/src/main/java/org/geektimes/web/mvc/demo/jolokia/JolokiaDemo.java
+++ projects/stage-0/user-platform/my-web-mvc/src/main/java/org/geektimes/web/mvc/demo/jolokia/JolokiaDemo.java
@@ -13,6 +13,7 @@ public class JolokiaDemo {
         ObjectName frontControllerServletName = new ObjectName("jolokia:name=FrontControllerServlet");
         server.registerMBean(new FrontControllerServlet(), frontControllerServletName);
         // 什么原理报错了？ Exception in thread "main" java.lang.NoClassDefFoundError: javax/servlet/http/HttpServlet
+        // https://stackoverflow.com/questions/19347604/mbeans-operaciones-error-weblogic
 
         System.out.println("start waiting.....");
         Thread.sleep(Long.MAX_VALUE);
