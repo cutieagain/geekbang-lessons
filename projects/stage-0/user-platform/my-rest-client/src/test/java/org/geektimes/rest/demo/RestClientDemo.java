65fda73facff10e2ca025c03a3641723a9ce5d84
cutie <cutieagain@gmail.com>
2021-03-31 11:42:18 +0800

    my-rest-client POST 方法

--- projects/stage-0/user-platform/my-rest-client/src/test/java/org/geektimes/rest/demo/RestClientDemo.java
+++ projects/stage-0/user-platform/my-rest-client/src/test/java/org/geektimes/rest/demo/RestClientDemo.java
@@ -2,17 +2,30 @@ package org.geektimes.rest.demo;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
 public class RestClientDemo {
 
     public static void main(String[] args) {
+        //GET
+//        Client client = ClientBuilder.newClient();
+//        Response response = client
+////                .target("http://127.0.0.1:8080/hello/world")      // WebTarget
+//                .request() // Invocation.Builder
+//                .get();                                     //  Response
+//
+//        String content = response.readEntity(String.class);
+//
+//        System.out.println(content);
+
+
+        //POST
         Client client = ClientBuilder.newClient();
         Response response = client
-                .target("http://127.0.0.1:8080/hello/world")      // WebTarget
-                .request() // Invocation.Builder
-                .get();                                     //  Response
-
+                .target("http://127.0.0.1:8080/hello/world")
+                .request()
+                .post(Entity.text(String.class));
         String content = response.readEntity(String.class);
 
         System.out.println(content);
